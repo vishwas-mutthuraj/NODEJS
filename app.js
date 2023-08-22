813a26669c83cd656c207db6aee4c0bf96049ed6
vishwas-mutthuraj <vishwas.mutthuraj@gmail.com>
2023-08-22 13:58:57 +0530

    adding firstExample of EventLoop

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const _ = require('lodash')
+// const _ = require('lodash')
 
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems);
+// const items = [1,[2,[3,[4]]]]
+// const newItems = _.flattenDeep(items)
+// console.log(newItems);
+// console.log('hello vishwas');
+
+// EventLoop Examples
+// const { readFile } = require('fs');
+// const { result } = require('lodash');
+// console.log('started a first task');
+// readFile('./content/first.txt', 'utf8', (err, result) => {
+//     if(err) {
+//         console.log(err);
+//         return
+//     }
+//     console.log(result);
+//     console.log('completed first task');
+// })
+// console.log('starting next task');
