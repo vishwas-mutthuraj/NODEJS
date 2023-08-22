7c8bdfbeea15bbce4fe088a21c1f24b05256ded5
vishwas-mutthuraj <vishwas.mutthuraj@gmail.com>
2023-08-22 11:47:43 +0530

    adding node project

---
+++ modules.js
@@ -0,0 +1,7 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative-flavor')
+require('./mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
